d9ed1e59db488313d1782ee739c3fe7542ff6949
christinekarimi <karimikim3@gmail.com>
2021-06-03 17:37:22 +0300

    Deploy to heroku

---
+++ app/models.py
@@ -0,0 +1,39 @@
+class Review:
+
+    all_reviews = []
+
+    def __init__(self,movie_id,title,imageurl,review):
+        self.movie_id = movie_id
+        self.title = title
+        self.imageurl = imageurl
+        self.review = review
+
+    @classmethod
+    def get_reviews(cls,id):
+
+        response = []
+
+        for review in cls.all_reviews:
+            if review.movie_id == id:
+                response.append(review)
+
+        return response
+
+
+    def save_review(self):
+        Review.all_reviews.append(self)
+
+
+    @classmethod
+    def clear_reviews(cls):
+        Review.all_reviews.clear()
+
+class Movie:
+
+    def __init__(self,id,title,overview,poster,vote_average,vote_count):
+        self.id =id
+        self.title = title
+        self.overview = overview
+        self.poster = 'https://image.tmdb.org/t/p/w500/'+ poster
+        self.vote_average = vote_average
+        self.vote_count = vote_count
---
+++ start.sh
@@ -0,0 +1,7 @@
+export MOVIE_API_KEY='ec358d808d5cf89f70eef77f99b40fc7'
+export SECRET_KEY='ckk'
+
+
+python3.7 manage.py server
+
+
